eae02b36769bbf40b9fc9e9ab3d80b959efab9f7
cevs89 <cevsinformatica@gmail.com>
2024-02-23 14:29:28 -0500

    Formating linters all files

--- imagine/urls.py
+++ imagine/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from drf_yasg import openapi
@@ -33,7 +34,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path(
         "docs/",
         schema_view.with_ui("swagger", cache_timeout=0),
